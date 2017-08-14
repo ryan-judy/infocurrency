229a728316fdb25b9cd94b5f0ae856fdbeb6d1b8
ryan-judy <rj113909@gmail.com>
2017-08-13 20:46:32 -0400

    changed file structure and tweaked more

---
+++ assets/js/test.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+    $(".nextPage").on("click", function () {
+		if ($("input[value=man]:checked").val() === "man") {
+			console.log("man")
+		}
+
+		else if ($("input[value=woman]:checked").val() === "woman") {
+			console.log("click")
+		}
+
+  });
+
+
+	//Switcher function:
+	$(".rb-tab").click(function(){
+	  //Spot switcher:
+	  $(this).parent().find(".rb-tab").removeClass("rb-tab-active");
+	  $(this).addClass("rb-tab-active");
+	});
+// if user age = null, then display demographic survey and hide the interest survey
+
+// else if user movies = null, then display interest survey and hide demographic survey
+
+// else write out Check back in for additional surveys and see if your information has been purchased
+
+
+});
